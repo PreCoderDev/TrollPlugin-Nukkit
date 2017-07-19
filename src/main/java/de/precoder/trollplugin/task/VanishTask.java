683458832d6b970e4cb8aedfec5133adba0d3bdc
PreCoder <PreCoderDev@users.noreply.github.com>
2017-07-19 17:52:55 +0200

    Create VanishTask.java

---
+++ src/main/java/de/precoder/trollplugin/task/VanishTask.java
@@ -0,0 +1,29 @@
+package de.precoder.trollplugin.task;
+
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.plugin.Plugin;
+import cn.nukkit.scheduler.PluginTask;
+import de.precoder.trollplugin.listener.TrollListener;
+
+public class VanishTask extends PluginTask<Plugin> {
+
+	public VanishTask(Plugin owner) {
+		super(owner);
+	}
+	
+	
+	@Override
+	public void onRun(int arg0) {
+		
+		for (Player hideplayer : Server.getInstance().getOnlinePlayers().values()) {
+			if(TrollListener.isPlayerVanished(hideplayer)) {
+				for (Player all : Server.getInstance().getOnlinePlayers().values()) {
+					all.hidePlayer(hideplayer);
+				}
+			}
+		}
+		
+	}
+
+}
